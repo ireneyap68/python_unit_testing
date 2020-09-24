c70afdd63e18f65364f24f1be913a447d1bfcfdc
Irene <airine@Irenes-MacBook-Air.local>
2020-09-24 12:32:16 -0700

    [test_calc] Add test function for sub,mul,div and pass test

--- test_calc.py
+++ test_calc.py
@@ -8,5 +8,20 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.add(100, 1), 101)
         self.assertEqual(calc.add(-88, 88), 0)
 
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10,5), 5)
+        self.assertEqual(calc.subtract(100, 1), 99)
+        self.assertEqual(calc.subtract(-88, 88), -176)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10,5), 2)
+        self.assertEqual(calc.divide(100, 1), 100)
+        self.assertEqual(calc.divide(-88, 88), -1)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10,5), 50)
+        self.assertEqual(calc.multiply(100, 1), 100)
+        self.assertEqual(calc.multiply(-88, 88), -7744)
+
 if __name__ == '__main__':
     unittest.main()
