e0e8928dd4d48cc4da720ea89560801133b33a84
Irene <airine@Irenes-MacBook-Air.local>
2020-09-24 12:08:02 -0700

    [calc] Add simple function on calculation

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    """Add Function"""
+
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+
+    return x - y
+
+def multiply(x, y):
+
+    return x * y
+
+def divide(x, y):
+
+    return x / y
